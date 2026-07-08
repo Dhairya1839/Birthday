0d33897c80cda8e34eb2468d974edb3b4091e8b9
Dhairya <dhairyabhanushali3535@gmail.com>
2026-07-08 19:59:21 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -166,7 +166,7 @@
             ctx.stroke();
 
             ctx.moveTo(0, 0);
-            ctx.scale(1.75, 1.75);
+            ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
             ctx.fillText("Birthday Queen !", 28, 10);
